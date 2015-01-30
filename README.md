8b83e115daabd1494c250d6b541e6996335f543c
Tolga HOŞGÖR <fasdfasdas@gmail.com>
2015-01-30 13:15:36 +0200

    added license texts and files
    Signed-off-by: Tolga HOŞGÖR <fasdfasdas@gmail.com>

---
+++ README.md
@@ -0,0 +1,8 @@
+libashttp
+=========
+
+An asynchronous HTTP library using Boost.ASIO.
+
+This project is licensed under:
+
+[![LGPLv3](http://www.gnu.org/graphics/lgplv3-147x51.png)](http://www.gnu.org/licenses/lgpl-3.0.html)
--- ashttp/type.cpp
+++ ashttp/type.cpp
@@ -4,6 +4,23 @@
  * File created on: 28.01.2015
 */
 
+/*
+  This file is part of libashttp.
+
+  libashttp is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  libashttp is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with libashttp.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "type.hpp"
 
 #include <boost/asio/error.hpp>
